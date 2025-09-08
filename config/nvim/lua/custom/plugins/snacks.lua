9c1f44557c52d3588fea14a89d420a43e60f9abf
robfs <robfoxsimms@gmail.com>
2025-09-08 15:13:09 +0100

    add neotest config

--- config/nvim/lua/custom/plugins/snacks.lua
+++ config/nvim/lua/custom/plugins/snacks.lua
@@ -62,7 +62,7 @@ return {
         desc = 'Command History',
       },
       {
-        '<leader>n',
+        '<leader>fn',
         function()
           Snacks.picker.notifications()
         end,
